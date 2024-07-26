3d3dd78a56fcfe8b189e6aa0f8e9cb8bdcb065ff
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-26 10:37:12 -0300

    Criando componente Filmes

--- filme/src/app/app.module.ts
+++ filme/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmesComponent } from './filmes/filmes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmesComponent
   ],
   imports: [
     BrowserModule,
